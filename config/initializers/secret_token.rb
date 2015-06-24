bfc9c83615edf86bac77a4ca19143943b9736b8a
Luis <luismetzger@me.com>
2015-06-24 16:38:37 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GschoolEducationApp::Application.config.secret_key_base = '949ef37918ab0681aef06373fc3aa83c0ea20e1fee5f03afd0d5980b00ca832a756ddb35317f9058d48786a94eb0b50063e04763bd57b27e539c74413b676405'
